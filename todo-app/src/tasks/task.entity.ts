45bc56ab949e4889e5a01e8f45b8dac2bc855eeb
Vikram Sharma <vikram.sharma1024@gmail.com>
2021-03-19 10:34:36 -0400

    comment

--- todo-app/src/tasks/task.entity.ts
+++ todo-app/src/tasks/task.entity.ts
@@ -14,3 +14,14 @@ export class Task{
     @Column()
     completed: boolean;
 }
+/*
+    TO ENTER DATA VIA POSTMAN:
+    1. Select Body next to Headers and Pre-Request Script
+    2. Select raw, select JSON from dropdown list to the right
+    3. Enter data as such:
+        {
+            "taskString": "This is the first task to add",
+            "assigned": "2021-03-19",
+            "completed": true
+        }
+*/
--- todo-app/src/tasks/tasks/tasks.controller.ts
+++ todo-app/src/tasks/tasks/tasks.controller.ts
@@ -2,21 +2,24 @@ import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common'
 import { Task } from '../task.entity';
 import { TasksService } from '../tasks.service';
 
-
+// all of your API routes will be off of the tasks controller. see comments below
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
+    // This means to get all tasks, you will hit "localhost:3000/tasks/" in postman
     @Get()
     index(): Promise<Task[]>{
         return this.tasksService.findAll()
     }
 
+    // This means to edit a task, you will hit "localhost:3000/tasks/create" in postman
     @Post('create')
     async create(@Body() taskData: Task): Promise<any>{
         return this.tasksService.create(taskData)
     }
 
+    // To update a task, hit "localhost:3000/tasks/1/update" or any other ID
     @Put(':id/update')
     async update(@Param('id') id, @Body() taskData: Task): Promise<any>{
         taskData.id = Number(id);
@@ -24,6 +27,7 @@ export class TasksController {
         return this.tasksService.update(taskData)
     }
 
+    // To delete a task, hit "localhost:3000/tasks/1/delete" or any other ID
     @Delete(':id/delete')
     async delete(@Param('id') id): Promise<any>{
         return this.tasksService.delete(id);
